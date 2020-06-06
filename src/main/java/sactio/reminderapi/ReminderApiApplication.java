7e49b2db0ec53a95990d45ca7701eea114d08e81
sactio.swastioyono <sactio.swastioyono@rakuten.com>
2020-06-06 15:06:41 +0900

    Initial commit

---
+++ src/main/java/sactio/reminderapi/ReminderApiApplication.java
@@ -0,0 +1,13 @@
+package sactio.reminderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sactio/reminderapi/ReminderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sactio.reminderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
