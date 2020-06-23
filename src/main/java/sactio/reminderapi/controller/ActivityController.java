ae9ee9ee39f90efd3fea2d603576ec8357a62c50
Sactio Swastioyono <email.tyo@gmail.com>
2020-06-23 21:48:22 +0900

    add open api (again)

--- src/main/java/sactio/reminderapi/controller/ActivityController.java
+++ src/main/java/sactio/reminderapi/controller/ActivityController.java
@@ -1,6 +1,5 @@
 package sactio.reminderapi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
