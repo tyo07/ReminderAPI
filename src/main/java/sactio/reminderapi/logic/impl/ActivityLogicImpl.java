325d2e04bb03f08a18276ef07c6373641805f08e
Sactio Swastioyono <email.tyo@gmail.com>
2020-07-24 21:32:03 +0900

    minor update

--- src/main/java/sactio/reminderapi/logic/impl/ActivityLogicImpl.java
+++ src/main/java/sactio/reminderapi/logic/impl/ActivityLogicImpl.java
@@ -1,6 +1,5 @@
 package sactio.reminderapi.logic.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import sactio.reminderapi.dto.ActivityDto;
 import sactio.reminderapi.dto.ActivityRequestDto;
